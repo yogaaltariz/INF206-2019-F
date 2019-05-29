fed3d5a310945fb469e7cec7e1682291f05949af
Naila Suqya <nailasuqya12@gmail.com>
2019-05-29 17:54:12 +0700

    tambah route add petugas

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -20,9 +20,6 @@ function checkSignIn(req, res,next){
 	}
 }
 
-
-
-
 const transporter = nodemailer.createTransport({
     pool: true,
     host: "smtp.gmail.com",
@@ -55,4 +52,39 @@ router.post('/petugas/edit/:id/save',checkSignIn,function(req,res){
             res.redirect(`/petugas/info/${req.params.id}`)
         }
     })
+})
+router.post('/addPetugas',function(req,res){
+	const password = Math.random().toString(36).substring(7);
+	bcrypt.hash(password,saltRounds,function(err,hash){
+		 const petugas = new Petugas({
+			  NIP: req.body.NIP,
+			  nama: req.body.nama,
+			  jk: req.body.jk,
+			  alamat: req.body.alamat,
+			  email: req.body.email,
+			  username: req.body.username,
+			  password: hash
+		 })
+		 petugas.save(function (err){
+			  if(err){
+					console.log(err)
+			  } else {
+					let message = {
+						 from: 'Ekir Project',
+						 to: req.body.email,
+						 subject: 'Daftar akun',
+						 text: `Selamat bergabung ${req.body.nama}, password akun anda : ${password}`
+					}
+	
+					transporter.sendMail(message,function(err){
+						 if(err){
+							  console.log(err)
+						 } else{
+							  req.flash('info', 'Berhasil menambah petugas');
+							  res.redirect('/petugas')
+						 }
+					})
+			  }
+		 })
+	})
 })
