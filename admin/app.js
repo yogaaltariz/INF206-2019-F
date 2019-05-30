257aa7a336b7c6bc95de6795080c8d2663d837b5
Yoga Altariz <yoga.altariz@gmail.com>
2019-05-30 20:52:30 +0700

    add session

--- admin/app.js
+++ admin/app.js
@@ -1,20 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
+const flash = require('express-flash')
 
-
-
-
-
-
-
-
-
+const app = express()
 
 app.use(express.static(__dirname+'/assets'));
 app.use(express.static(__dirname+'/css'));
 app.use(express.static(__dirname+'/js'));
+//set view engine menggunakan ejs
+app.set("view engine","ejs")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(session({
+	secret: "This is my secret", //save it to dotenv
+	resave: false,
+	saveUninitialized: false
+}))
 
-
-
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 mongoose.connect("mongodb://localhost:27017/ekir",{useNewUrlParser: true})
 
